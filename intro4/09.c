00a806696905922718a58d3435b30cb28e442046
Yuta Sugii <sugii@nowhere.co.jp>
2015-12-17 17:20:48 +0900

    [fix]finish to correspond Open-MP🔥

--- intro4/09.c
+++ intro4/09.c
@@ -17,5 +17,26 @@ int main(void)
 また、それらの指示文にはorderd指示節を付ける必要があります。
 
 上のプログラムを実行スレッド数4で実行すると上記のように表示されます。
-
+$ g++ 09.c -fopenmp
+ i=0 thread_num=0
+ i=1 thread_num=0
+ i=2 thread_num=0
+ ...
+ i=23 thread_num=0
+ i=24 thread_num=0
+ i=25 thread_num=1
+ i=26 thread_num=1
+ ...
+ i=48 thread_num=1
+ i=49 thread_num=1
+ i=50 thread_num=2
+ i=51 thread_num=2
+ ...
+ i=73 thread_num=2
+ i=74 thread_num=2
+ i=75 thread_num=3
+ i=76 thread_num=3
+ ...
+ i=98 thread_num=3
+ i=99 thread_num=3
 */
